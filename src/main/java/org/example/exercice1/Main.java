27ee284fbdc34ba8b52f04ed9f0f79e2a2d68bf1
unknown <elm.dev.code@gmail.com>
2024-10-16 12:08:02 +0200

    exercice 1

---
+++ src/main/java/org/example/exercice1/Main.java
@@ -0,0 +1,17 @@
+package org.example.exercice1;
+
+public class Main {
+    public static void main(String[] args) {
+        GradingCalculator calc1 = new GradingCalculator(95, 90);
+        System.out.println("Grade avec Score 95 et Attendance 90: " + calc1.getGrade());
+
+        GradingCalculator calc2 = new GradingCalculator(85, 90);
+        System.out.println("Grade avec Score 85 et Attendance 90: " + calc2.getGrade());
+
+        GradingCalculator calc3 = new GradingCalculator(65, 90);
+        System.out.println("Grade avec Score 65 et Attendance 90: " + calc3.getGrade());
+
+        GradingCalculator calc4 = new GradingCalculator(95, 65);
+        System.out.println("Grade avec Score 95 et Attendance 65: " + calc4.getGrade());
+    }
+}
---
+++ src/test/java/org/example/exercice1/GradingCalculatorTest.java
@@ -0,0 +1,56 @@
+package org.example.exercice1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GradingCalculatorTest {
+
+    @Test
+    public void whenScore95AndAttendance90_GradeA() {
+        GradingCalculator calc = new GradingCalculator(95, 90);
+        char grade = calc.getGrade();
+        Assert.assertEquals('A', grade);
+    }
+
+    @Test
+    public void whenScore85AndAttendance90_GradeB() {
+        GradingCalculator calc = new GradingCalculator(85, 90);
+        char grade = calc.getGrade();
+        Assert.assertEquals('B', grade);
+    }
+
+    @Test
+    public void whenScore65AndAttendance90_GradeC() {
+        GradingCalculator calc = new GradingCalculator(65, 90);
+        char grade = calc.getGrade();
+        Assert.assertEquals('C', grade);
+    }
+
+    @Test
+    public void whenScore95AndAttendance65_GradeB() {
+        GradingCalculator calc = new GradingCalculator(95, 65);
+        char grade = calc.getGrade();
+        Assert.assertEquals('B', grade);
+    }
+
+    @Test
+    public void whenScore95AndAttendance55_GradeF() {
+        GradingCalculator calc = new GradingCalculator(95, 55);
+        char grade = calc.getGrade();
+        Assert.assertEquals('F', grade);
+    }
+
+    @Test
+    public void whenScore65AndAttendance55_GradeF() {
+        GradingCalculator calc = new GradingCalculator(65, 55);
+        char grade = calc.getGrade();
+        Assert.assertEquals('F', grade);
+    }
+
+    @Test
+    public void whenScore50AndAttendance90_GradeF() {
+        GradingCalculator calc = new GradingCalculator(50, 90);
+        char grade = calc.getGrade();
+        Assert.assertEquals('F', grade);
+    }
+}
