72fdb9fa93fc3e427797afd99ad312cf4d4954bf
Amine <elm.dev.code@gmail.com>
2024-10-17 10:40:25 +0200

    test (exercice4/test) : Implement 'testSearchLessTwoCharactersThrowsException' test case

--- src/test/java/org/example/exercice4/RechercheVilleTest.java
+++ src/test/java/org/example/exercice4/RechercheVilleTest.java
@@ -1,1 +1,18 @@
 package org.example.exercice4;
+
+import org.example.exercice4.exception.NotFoundException;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class RechercheVilleTest {
+
+    @Test
+    public void testSearchLessTwoCharactersThrowsException() {
+        RechercheVille rechercheVille = new RechercheVille();
+        Assert.assertThrows(NotFoundException.class, () -> {
+            rechercheVille.rechercher("A");
+        });
+    }
+}
+
